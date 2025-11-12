ef0d58ada47f9d99d70eff11211a88c2cd359954
Dinesh Mudrakola <dmudrakola@google.com>
2025-11-12 00:18:25 +0000

    Feature: Pushed V4 update to test automated deployment

--- gcp-gitops-platform/app-repo/server.js
+++ gcp-gitops-platform/app-repo/server.js
@@ -2,4 +2,4 @@
  * @fileoverview Description of this file.
  */
 # Example: update a line in your application file
-sed -i '' 's/Hello, GitOps V2!/Hello, GitOps V3!/g' server.js
+sed -i '' 's/Hello, GitOps V3!/Hello, GitOps V4!/g' server.js
